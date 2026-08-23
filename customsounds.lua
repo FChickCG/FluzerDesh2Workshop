bb89e972a4fd454fb4f553937673da09d7b6f302
FChickCG <floppa.youtuber@yandex.ru>
2026-08-23 16:25:59 +0500

    Delete customsounds.lua

--- customsounds.lua
+++
@@ -1,6 +0,0 @@
-function init()
-  api.log("Мод загружен!")
-  api.set_jump_sound("https://example.com/jump.mp3")
-  api.set_damage_sound("https://example.com/hit.wav")
-  api.get_author()   -- ник автора из index.json
-end
